a5b79c29be5636ada206b9a3a03054bdf232a880
RodrigoFrancaBR <rfrancacosta@gmail.com>
2021-02-06 15:37:07 -0200

    gitignore update

---
+++ desafio-NTConsult/src/main/java/br/com/ntconsult/DesafioNtConsultApplication.java
@@ -0,0 +1,14 @@
+package br.com.ntconsult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNtConsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNtConsultApplication.class, args);
+		System.out.println("Hello World Spring Boot");
+	}
+
+}
---
+++ desafio-NTConsult/src/test/java/br/com/ntconsult/DesafioNtConsultApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ntconsult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNtConsultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
